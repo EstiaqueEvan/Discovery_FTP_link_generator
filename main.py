c7385120a5d07de82221c08412f33f616db7efa7
EstiaqueEvan <72780620+EstiaqueEvan@users.noreply.github.com>
2023-01-15 23:22:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from lib import *
-from time import sleep
-from custom_ui import *
 import requests
+from lib import login, logout, search, link_crawler
+from custom_ui import print_ui, user_warning, items_list
+from time import sleep
+from bs4 import BeautifulSoup
+
+def generate_links(session, search_type, search_term):
+    items = search(session, search_type, search_term)
+    items_list(items, search_type)
+    selection = int(input('Enter the item count to generate links:'))
+    print('\n\n')
+    if search_type == 'series':
+        crawled_links = link_crawler(session=session, select_url=items[selection][1]['item_url'] + '/')
+        item_name = items[selection][1]['item_name']
+        with open(f'GeneratedLinks/{item_name}_links.txt', 'a+') as file:
+            for link in crawled_links:
+                file.write(link + '\n')
+            file.close()
+        print(f'Links generated for {item_name}')
+    elif search_type == 'movies':
+        movie_player = session.get(url=primary_url + items[selection][1]['item_url'], headers=dflix_headers)
+        movie_player_soup = BeautifulSoup(movie_player.text, "html.parser")
+        item_name = items[selection][1]['item_name']
+        with open(f'GeneratedLinks/{item_name}_links.txt', 'a+') as file:
+            file.write(movie_player_soup.find('source')['src'] + '\n')
+            file.close()
+        print(f'Links generated for {item_name}')
+    print('Saved to GeneratedLinks folder.')
+    sleep(1)
+    clear()
+
 def main():
     with requests.Session() as session:
         print_ui()
@@ -12,52 +39,22 @@ def main():
     while True:
         get_profile_info(session)
         print_ui()
+        choice = input("Enter your choice: ")
         try:
-            choice = int(input("Enter your choice: "))
-        except:
+            choice = int(choice)
+        except ValueError:
             print("INVALID CHOICE!")
             sleep(1)
             clear()
             print("Returning to the main menu!")
             sleep(1)
-            clear()
             continue
-        if choice == 1:#re-login
+        if choice == 1:  # re-login
             relogin(session)
-        elif choice == 2:#generate links: series
-            search_type = 'series'
-            search_term = input("Enter any word to search: ")
-            items = search(session,search_type,search_term)
-            items_list(items = items, search_type = search_type)
-            selection = int(input('Enter the item count to generate links:'))
-            print('\n\n')
-            crawled_links = link_crawler(session = session, select_url = items[selection][1]['item_url'] + '/')
-            item_name = items[selection][1]['item_name']
-            for links in crawled_links:
-                with open(f'''GeneratedLinks/{item_name}_links.txt''','a+') as file:
-                    file.write(links + '\n')
-                    file.close()
-            print(f'Links generated for {item_name}')
-            print('Saved to GeneratedLinks folder.')
-            sleep(1)
-            clear()
-        elif choice == 3:#generate links: movies
-            search_type = 'movies'
-            search_term = input("Enter any word to search: ")
-            items = search(session,search_type,search_term)
-            items_list(items,search_type)
-            selection = int(input('Enter the item count to generate links:'))
-            print('\n\n')
-            movie_player = session.get(url = primary_url + items[selection][1]['item_url'], headers = dflix_headers)
-            movie_player_soup = BeautifulSoup(movie_player.text, "html.parser")
-            item_name = items[selection][1]['item_name']
-            with open(f'''GeneratedLinks/{item_name}_links.txt''','a+') as file:
-                file.write(movie_player_soup.find('source')['src'] + '\n')
-                file.close()
-            print(f'Links generated for {item_name}')
-            print('Saved to GeneratedLinks folder.')
-            sleep(1)
-            clear()
+        elif choice == 2:  # generate links: series
+            generate_links(session, 'series', input("Enter any word to search: "))
+        elif choice == 3:  # generate links: movies
+            generate_links(session, 'movies', input("Enter any word to search: "))
         elif choice == 4:
             logout(session)
             continue
@@ -68,5 +65,6 @@ def main():
             print('The program exited successfully!')
             break
     return 0
+
 if __name__ == '__main__':
     main()
